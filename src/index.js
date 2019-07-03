e66bd409b0c671b12bf127113b3ceb7860ebd031
Anush Shukla <anush.shukla@gmail.com>
2019-07-03 07:43:23 +0530

    base 64 conditional chunked dynamic imports

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ if (isBase64Supported) {
     encodeToBase64 = data => Buffer.from(data).toString('base64');
     decodeFromBase64 = b64string => Buffer.from(b64string, 'base64').toString('ascii');
 } else {
+    // @todo use import of ES6 instead of require of ES5
+    // using require instead of promised based dynamic imports till https://esdiscuss.org/topic/dynamic-synchronous-import is finalised
     encodeToBase64 = require('./encodeToBase64');
     decodeFromBase64 = require('./decodeFromBase64');
 }
